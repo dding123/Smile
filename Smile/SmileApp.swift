4d377173a60f08b8945ffabffc9c9b01b740bcad
David Ding <davding101@gmail.com>
2023-07-18 22:02:35 -0700

    Initial Commit

---
+++ Smile/SmileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmileApp.swift
+//  Smile
+//
+//  Created by David Ding on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SmileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
